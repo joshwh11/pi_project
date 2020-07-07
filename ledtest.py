98ead12ea520cceae72e6e486b81a0e114c5e0c5
joshwh11 <67963694+joshwh11@users.noreply.github.com>
2020-07-07 10:48:58 -0400

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -7,32 +7,29 @@ GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 
-#Turn on Red LED
+#Turn on/off Red LED
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(18,GPIO.LOW)
 
-#Turn on Yellow LED
+#Turn on/off Yellow LED
 GPIO.output(24,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(24,GPIO.LOW)
 
-#Turn on Red LED
+#Repeat
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(18,GPIO.LOW)
 
-#Turn on Yellow LED
 GPIO.output(24,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(24,GPIO.LOW)
 
-#Turn on Red LED
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(18,GPIO.LOW)
 
-#Turn on Yellow LED
 GPIO.output(24,GPIO.HIGH)
 time.sleep(1)
 GPIO.output(24,GPIO.LOW)
