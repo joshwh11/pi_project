df32ef52f2edc7befb50a24c4413d472e6724d65
joshwh11 <67963694+joshwh11@users.noreply.github.com>
2020-07-13 17:26:07 -0400

    Update requestscript.py

--- requestscript.py
+++ requestscript.py
@@ -6,7 +6,7 @@ import sys
 # For example, to turn the red pin on, you would use the command:
 # python3 requestscript.py 1 on
 
-url = 'http://localhost:9999/pins/' + str(sys.argv[1])
+url = 'http://localhost:5000/pins/' + str(sys.argv[1])
 payload = {'state': str(sys.argv[2])}
 
 rpatch = requests.patch(url, json=payload)
