1d69819bad4e877cdede04d92978fb90303ef213
joshwh11 <67963694+joshwh11@users.noreply.github.com>
2020-07-17 12:08:29 -0400

    Update checkHealth.py

--- checkHealth.py
+++ checkHealth.py
@@ -9,12 +9,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
-#Define url, token, and username
-url = "https://api.github.com/repos/joshwh11/pi_project/contents/syntheticHealth.txt"
-token = "8612723ac97777218d7effbd987266d61bc0b45f"
-usern = "joshwh11"
+# Define url with your values
+url = "https://api.github.com/repos/<your username>/<your repo>/contents/syntheticHealth.txt"
 
-#Get certain file content
+# Get certain file content
 print("Application running... to stop type control-C :)")
 while True:
     r = requests.get(url).text
